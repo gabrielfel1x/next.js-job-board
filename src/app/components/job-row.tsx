4819ce3951e17be9df8283657d9efde6667d7f1a
gabrielfel1x <gabrielfelxx@gmail.com>
2024-06-26 16:05:16 -0300

    customize job-row

--- src/app/components/job-row.tsx
+++ src/app/components/job-row.tsx
@@ -1,25 +1,34 @@
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function JobRow() {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-sm flex gap-4">
-      <div className="content-center">
-        <picture>
-          <img
-            className="size-16 p-2"
-            src="https://seeklogo.com/images/P/picpay-logo-A98DEA805B-seeklogo.com.png"
-            alt="test"
-          />
-        </picture>
+    <div className="bg-white p-6 rounded-lg shadow-sm relative">
+      <div className="absolute top-6 right-6 cursor-pointer">
+        <FontAwesomeIcon icon={faBookmark} className="size-6 text-gray-300" />
       </div>
+      <div className="flex grow gap-4">
+        <div className="content-center">
+          <picture>
+            <img
+              className="size-16 p-2"
+              src="https://seeklogo.com/images/P/picpay-logo-A98DEA805B-seeklogo.com.png"
+              alt="test"
+            />
+          </picture>
+        </div>
 
-      <div className="grow">
-        <div className="text-gray-500">PicPay</div>
-        <div className="font-bold text-lg">Software Developer</div>
-        <div className="text-gray-400 text-sm">
-          Remote &middot; Vitória, ES &middot; Full-time
+        <div className="grow sm:flex">
+          <div className="grow">
+            <div className="text-gray-500 cursor-pointer">PicPay</div>
+            <div className="font-bold text-lg">Software Developer</div>
+            <div className="text-gray-400 text-sm">
+              Remote &middot; Vitória, ES &middot; Full-time
+            </div>
+          </div>
+          <div className="content-end text-gray-400 text-sm">3 weeks ago</div>
         </div>
       </div>
-
-      <div className="content-end text-gray-400 text-sm">3 weeks ago</div>
     </div>
   );
 }
